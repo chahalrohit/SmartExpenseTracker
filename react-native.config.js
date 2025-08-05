8b46f56449a4bd283f0fcba39ef21b21758a3744
Rohit <rcrchahal@gmail.com>
2025-08-05 15:37:54 +0530

    bug fix

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  dependencies: {},
-  project: {
-    ios: {},
-    android: {},
-  },
+  // dependencies: {},
+  // project: {
+  //   ios: {},
+  //   android: {},
+  // },
   assets: ['./src/assets/fonts'],
 };
