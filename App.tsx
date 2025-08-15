5ed5141594a26960ad826d109d3fecc5c4b016bb
Rohit Chahal <rohitchahal@Rohits-Mac-mini.local>
2025-08-15 23:26:34 +0530

    ios fix

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar, PermissionsAndroid, Platform } from 'react-native';
 import AuthNavigation from './src/navigation/AuthNavigation';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 
+async function ensureNotifPermission() {
+  if (Platform.OS !== 'android' || Platform.Version < 33) return true;
+
+  const res = await PermissionsAndroid.request(
+    'android.permission.POST_NOTIFICATIONS',
+  );
+
+  return res === PermissionsAndroid.RESULTS.GRANTED;
+}
+
 export default function App() {
+  useEffect(() => {
+    ensureNotifPermission();
+  }, []);
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }} testID="app-root">
